f9f51461caa70741e1b5dbcb39225f96c042e3aa
ComeBackCity <gourabsaha567@gmail.com>
2022-01-02 18:57:05 +0600

    Updated missing number

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,10 +1,14 @@
 # Problem Link: https://leetcode.com/problems/missing-number/
+from typing import List
+
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         total = (n * (n + 1)) // 2
         array_total = sum(nums)
         return total - array_total
+
 
 if __name__ == '__main__':
     pass
