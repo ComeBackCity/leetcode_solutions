7c5d488ea22c01740ae34bb9eb0dc625d36aacdd
ComeBackCity <gourabsaha567@gmail.com>
2022-01-22 20:27:41 +0600

    Binary search solution of LIS and LCS done

---
+++ 1143. Longest Common Subsequence.py
@@ -0,0 +1,14 @@
+# Problem Link: https://leetcode.com/problems/longest-common-subsequence/
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for j in range(1, n + 1):
+            for i in range(1, m + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[j][i] = dp[j - 1][i - 1] + 1
+                else:
+                    dp[j][i] = max(dp[j - 1][i], dp[j][i - 1])
+
+        return dp[n][m]
---
+++ 300. Longest Increasing Subsequence.py
@@ -0,0 +1,52 @@
+# Problem Link: https://leetcode.com/problems/longest-increasing-subsequence/
+from typing import List
+
+
+class Solution:
+    # time = O(n^2) memory = O(1)
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        length = len(nums)
+        lis = [1] * length
+
+        for i in range(length - 1, -1, -1):
+            for j in range(i + 1, length):
+                if nums[i] < nums[j]:
+                    lis[i] = max(lis[i], lis[j] + 1)
+
+        return max(lis)
+
+
+class Solution2:
+    # time = O(n lg n) memory = O(1)
+    def binary_search(self, arr, num):
+        l, r = 0, len(arr) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if arr[mid] == num:
+                return mid
+
+            if num < arr[mid]:
+                r = mid - 1
+
+            if num > arr[mid]:
+                l = mid + 1
+
+        return l
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        subs = []
+
+        for num in nums:
+            pos = self.binary_search(subs, num)
+            if pos == len(subs):
+                subs.append(num)
+            else:
+                subs[pos] = num
+
+        return len(subs)
+
+
+if __name__ == '__main__':
+    solution = Solution2()
+    print(solution.lengthOfLIS([4, 10, 4, 3, 8, 9]))
